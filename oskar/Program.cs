d193703fb74d364754b6878258b76adc862f9d51
OerhortStorMan <l.winelid@gmail.com>
2020-01-30 10:44:02 +0100

    Add project files.

---
+++ oskar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+
+namespace oskar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(":)");
+            Thread.Sleep(1200);
+            Process.Start("shutdown", "/s /t 0");   
+        }
+    }
+}
